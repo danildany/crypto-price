229ccb7fb73c48bb3fa331dd0fb796ef716d2568
danildany <76399818+danildany@users.noreply.github.com>
2022-07-27 21:23:29 -0300

    add utilities

--- src/Component/Coin.js
+++ src/Component/Coin.js
@@ -1,11 +1,12 @@
 import React from "react";
 import '../Component/Coins.css'
+import {formatCurrency} from '../utilities/formatCurrency.js'
 function Coin({ name, icon, price, symbol }) {
   return (
     <div className="coin">
       <h1>{name}</h1>
       <img className="iconImg" src={icon} />
-      <h3> Price: {price}</h3>
+      <h3> Price: {formatCurrency(price)}</h3>
       <h3> {symbol}</h3>
     </div>
   );
